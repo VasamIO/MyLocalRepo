e2817f59f7bcdd28b8edb98bd9cad6920c8a4c6c
Vasam <venkatesh.vasam@cloudio.io>
2017-02-06 10:47:18 +0530

    init commit

--- git/install_hooks.sh
+++ git/install_hooks.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-HOOK_NAMES="commit-msg"
-# assuming the script is in a bin directory, one level into the repo
-HOOK_DIR=$(git rev-parse --show-toplevel)/.git/hooks
-
-for hook in $HOOK_NAMES; do
-    ln -s -f ../../bin/hooks-wrappe $HOOK_DIR/$hook
-done
+rm -rf ../.git/hooks/*
+cp -rf hooks/* ../.git/hooks/
+chmod +x ../.git/hooks/*
